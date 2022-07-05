578821cf78dbbb6da83dad55ed2499d67b8c1195
Nilay2800 <napatel@aakashinformatics.com>
2022-07-05 16:38:20 +0530

    ANNOUNCEMENT AND LOGIN DONE

---
+++ SMS/Controllers/AnnoucementController.cs
@@ -0,0 +1,65 @@
+﻿using SMS.Model;
+using SMS.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMS.Controllers
+{
+    public class AnnoucementController : Controller
+    {
+        public readonly AnnoucementService annoucementService;
+
+        public AnnoucementController()
+        {
+            annoucementService = new AnnoucementService();
+        }
+        // GET: Annoucement
+        public ActionResult Index()
+        {
+            List<AnnoucementModel> annoucements = annoucementService.GetAllAnnoucement();
+            return View(annoucements);
+        }
+
+        [HttpGet]
+        public ActionResult AddAnnoucement()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult AddAnnoucement(AnnoucementModel annoucementModel)
+        {
+            annoucementService.CreateAnnoucement(annoucementModel);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult EditAnnoucement(int id)
+        {
+
+            AnnoucementModel annoucementModel = annoucementService.GetAnnocementById(id);
+            return View(annoucementModel);
+        }
+
+
+        [HttpPost]
+        public ActionResult EditAnnoucement(AnnoucementModel annoucementModel)
+        {
+            AnnoucementModel annoucementModel1= annoucementService.UpdateAnnoucement(annoucementModel);
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Delete(int Id)
+        {
+
+            annoucementService.DeleteAnnoucement(Id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SMS/Controllers/TeacherController.cs
@@ -0,0 +1,63 @@
+﻿using SMS.Data.Database;
+using SMS.Model;
+using SMS.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMS.Controllers
+{
+    public class TeacherController : Controller
+    {
+        public readonly TeacherService teacherService;
+        public TeacherController()
+        {
+            teacherService = new TeacherService();
+        }
+        // GET: Teacher
+        public ActionResult Index()
+        {
+            List<TeacherModel> teachers = teacherService.GetAllTeacher();
+            return View(teachers);
+        }
+        [HttpGet]
+        public ActionResult AddTeacher()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult AddTeacher(TeacherModel teacher)
+        {
+            teacherService.CreateTeacher(teacher);
+            return RedirectToAction("Index","Teacher");
+        }
+        [HttpGet]
+        public ActionResult EditTeacher(int id)
+        {
+
+            TeacherModel teacherModel = teacherService.GetTeacherById(id);
+            return View(teacherModel);
+        }
+
+
+        [HttpPost]
+        public ActionResult EditTeacher(TeacherModel teacherModel)
+        {
+            TeacherModel teacherModel1 = teacherService.UpdateTeacher(teacherModel);
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Delete(int Id)
+        {
+
+            teacherService.DeleteTeacher(Id);
+            return RedirectToAction("Index");
+        }
+    }
+}
