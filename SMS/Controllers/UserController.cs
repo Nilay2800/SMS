63163acc51560b90328ae409e123ddcd91b45a7f
Nilay2800 <napatel@aakashinformatics.com>
2022-07-05 16:38:20 +0530

    ANNOUNCEMENT AND LOGIN DONE

---
+++ SMS/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using SMS.Data;
+using SMS.Data.Database;
+using SMS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace SMS.Controllers
+{
+    public class UserController : Controller
+    {
+        SMSContext smsContext = new SMSContext();
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(SignupModel model)
+        {
+            Signup s = new Signup();
+            s.Email = model.Email;
+            s.UserName = model.UserName;
+            s.Password = model.Password;
+            s.ConfirmPassword = model.ConfirmPassword;
+            smsContext.signups.Add(s);
+            smsContext.SaveChanges();
+            return RedirectToAction("Login");
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(SignupModel model)
+        {
+            Signup s = smsContext.signups.Where(a => a.Email.Equals(model.Email) && a.Password.Equals(model.Password)).SingleOrDefault();
+            if (s != null)
+            {
+                Session["UserId"] = s.Userid.ToString();
+                Session["UserName"] = s.UserName.ToString();
+                Session["Email"] = s.Email.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.msg = "Invalid Email or Password";
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
--- SMS/Global.asax.cs
+++ SMS/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SMS.Data;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,8 @@ namespace SMS
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //Database.SetInitializer<SMSContext>(new DropCreateDatabaseIfModelChanges<SMSContext>());
+            Database.SetInitializer<SMSContext>(null);
         }
     }
 }
