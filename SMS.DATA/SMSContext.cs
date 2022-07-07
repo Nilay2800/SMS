baf93f58bdb482d3c88e0c25bf8f0ff354f6b12c
Nilay2800 <napatel@aakashinformatics.com>
2022-07-07 12:46:41 +0530

    resolvcve

--- SMS.DATA/SMSContext.cs
+++ SMS.DATA/SMSContext.cs
@@ -1,4 +1,5 @@
 ﻿using SMS.Data.Database;
+using SMS.Model;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -13,6 +14,7 @@ namespace SMS.Data
         public DbSet<Teacher> teachers { get; set; }
         public DbSet<Annoucement> annoucements { get; set; }
         public DbSet<Signup> signups { get; set; }
+        public DbSet<Student> students { get; set; }
 
         public System.Data.Entity.DbSet<SMS.Model.SignupModel> SignupModels { get; set; }
     }
