05f6d0baf70bb2156f1882ea5ae5e7db5f946958
Nilay2800 <napatel@aakashinformatics.com>
2022-07-08 15:32:58 +0530

    Added-Rolemaster

---
+++ SMS.SERVICE/RoleMasterService.cs
@@ -0,0 +1,50 @@
+﻿using SMS.Data;
+using SMS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Service
+{
+    public class RoleMasterService
+    {
+        public readonly RoleMasterProvider roleMasterProvider;
+        public RoleMasterService()
+        {
+            roleMasterProvider = new RoleMasterProvider();
+        }
+        public List<WebpagesRole> GetAllRoles()
+        {
+            var roles = roleMasterProvider.GetAllRoles();
+            return roles;
+        }
+        //public WebpagesRole GetRolesById(int id)
+        //{
+        //    return roleMasterProvider.GetRolesById(id);
+        //}
+
+        public WebpagesRole GetRolesById()
+        {
+            var data = roleMasterProvider.GetRolesById();
+            WebpagesRole role = new WebpagesRole()
+            {
+                RoleId = data.RoleId,
+                RoleName = data.RoleName,
+                RoleCode = data.RoleCode,
+                IsActive = data.IsActive
+            };
+            return role;
+        }
+        public WebpagesRole GetRolesByName(string roleName)
+        {
+            return roleMasterProvider.GetRolesByName(roleName);
+        }
+
+        public WebpagesRole CreateRole(WebpagesRole role)
+        {
+            return roleMasterProvider.CreateRole(role);
+        }
+    }
+}
---
+++ SMS/Controllers/RoleMasterController.cs
@@ -0,0 +1,36 @@
+﻿using SMS.Model;
+using SMS.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMS.Controllers
+{
+    public class RoleMasterController : Controller
+    {
+        private readonly RoleMasterService roleMasterService;
+
+        public RoleMasterController()
+        {
+            roleMasterService = new RoleMasterService();
+        }
+
+        public ActionResult Index()
+        {
+            List<WebpagesRole> RoleList = roleMasterService.GetAllRoles();
+            return View(RoleList);
+        }
+        public ActionResult CreateRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateRole(WebpagesRole role)
+        {
+            roleMasterService.CreateRole(role);
+            return RedirectToAction("Index");
+        }
+    }
+}
