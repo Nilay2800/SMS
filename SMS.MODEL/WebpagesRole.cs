489374611698866df8f9e0bc3d2f9d2ea370aabd
Nilay2800 <napatel@aakashinformatics.com>
2022-07-08 12:42:27 +0530

    added usermaster

---
+++ SMS.MODEL/WebpagesRole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Model
+{
+    public class WebpagesRole
+    {
+        [Key]
+        public int RoleId { get; set; }
+        [Display(Name = "Role Name")]
+        [Required]
+        public string RoleName { get; set; }
+        public bool IsActive { get; set; }
+        [Display(Name = "Role Code")]
+        [Required]
+        public string RoleCode { get; set; }
+        
+    }
+}
---
+++ SMS.SERVICE/UserMasterService.cs
@@ -0,0 +1,43 @@
+﻿using SMS.Data;
+using SMS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Service
+{
+    public class UserMasterService
+    {
+        private readonly UserMasterProvider userMasterProvider;
+        public UserMasterService()
+        {
+            userMasterProvider = new UserMasterProvider();
+        }
+        public List<Signups> GetAllUser()
+        {
+            return userMasterProvider.GetAllUser();
+        }
+
+        public Signups GetUserById(int id)
+        {
+            return userMasterProvider.GetUserById(id);
+        }
+
+        public Signups UpdateUsersRole(Signups pur)
+        {
+            return userMasterProvider.UpdateUsersRole(pur);
+        }
+
+        public List<DropDownList> BindRole()
+        {
+            return userMasterProvider.BindRole();
+        }
+
+        public Signups DeleteUser(int id)
+        {
+            return userMasterProvider.DeleteUser(id);
+        }
+    }
+}
